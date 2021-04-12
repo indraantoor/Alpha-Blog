302e8739ef79e4e6d259b60e6bff21db009163f5
Indraan Toor <indraantoor@gmail.com>
2021-04-13 00:31:27 +0530

    Added Secure Password And Attribute To Users

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
+gem 'bcrypt', '~> 3.1.7'
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 5.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_182253) do
+ActiveRecord::Schema.define(version: 2021_04_12_185929) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_182253) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
