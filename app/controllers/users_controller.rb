9b9e8b51f1963a5ab557815aed9711881b83eb9c
Indraan Toor <indraantoor@gmail.com>
2021-04-17 15:11:03 +0530

    Added SignIn Upon Sign Up Functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:success] = "Welcome To The Alpha Blog #{@user.username}"
-			redirect_to articles_path
+			redirect_to users_path(@user)
 		else
 			render 'new'
 		end
